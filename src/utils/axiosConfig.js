09d43a01f0dbe91b01e0a11866f684038f3a21b3
Rama Almer Felix <quenzvezda@gmail.com>
2024-04-13 12:10:25 +0700

    Change API url to Env Variable

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// Menggunakan environment variable dengan nilai default
+const apiHost = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000';
+
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:5000/api',
+    baseURL: `${apiHost}/api`,
 });
 
 export default axiosInstance;
